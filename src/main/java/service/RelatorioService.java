835adc2532e5a54014fd279d4a6bf00052202d23
FERNANDO ELLERY <fernando.hle@gmail.com>
2025-04-11 09:08:17 -0300

    Update RelatorioService.java

--- src/main/java/service/RelatorioService.java
+++ src/main/java/service/RelatorioService.java
@@ -1,1 +1,21 @@
+package com.seuusuario.relatorio.service;
 
+import net.sf.jasperreports.engine.*;
+import java.util.HashMap;
+
+public class RelatorioService {
+    public void gerarRelatorio() {
+        try {
+            JasperReport report = JasperCompileManager.compileReport("src/main/resources/templates/relatorio.jrxml");
+            JasperPrint print = JasperFillManager.fillReport(report, new HashMap<>(), new JREmptyDataSource());
+            JasperExportManager.exportReportToPdfFile(print, "relatorio-gerado.pdf");
+            System.out.println("Relatório gerado com sucesso!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new RelatorioService().gerarRelatorio();
+    }
+}
